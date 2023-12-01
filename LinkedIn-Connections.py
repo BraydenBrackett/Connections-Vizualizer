574cec9e6ddccd6288be8d3e94331fdbaa8e6b0d
BraydenBrackett <bbracket@purdue.edu>
2023-11-30 20:11:17 -0500

    Added simplification

--- LinkedIn-Connections.py
+++ LinkedIn-Connections.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import tkinter as tk
 from tkinter import filedialog
+import recordlinkage
 import os
 
 class LinkedIn_Connections:
@@ -39,25 +40,68 @@ class LinkedIn_Connections:
         for file_path in self.file_paths:
             self.listbox.insert(tk.END, file_path)
 
+
+    #function for simplifing down companies
+    def simplify_companies(self, df):
+        # Create an indexer and compare the company names using string similarity
+        indexer = recordlinkage.Index()
+        indexer.full()
+        candidates = indexer.index(df)
+
+        compare_cl = recordlinkage.Compare()
+
+        # String similarity comparison
+        compare_cl.string('Company', 'Company', method='jarowinkler', threshold=0.8)
+
+        # Compute the similarity scores
+        features = compare_cl.compute(candidates, df)
+
+        # Define a threshold for considering matches
+        threshold = 0.8
+
+        # Get pairs of similar records based on the threshold
+        matches = features[features.sum(axis=1) > threshold]
+
+        # Create a dictionary to map original company names to simplified names
+        company_mapping = {}
+
+        # Update the mapping with the pairs of similar records
+        for idx1, idx2 in matches.index:
+            name1 = df.loc[idx1, 'Company']
+            name2 = df.loc[idx2, 'Company']
+            
+            if name1 not in company_mapping:
+                company_mapping[name1] = name1
+            
+            if name2 not in company_mapping:
+                company_mapping[name2] = name1
+
+        # Apply the mapping to create a new 'Simplified Company' column
+        df['Simplified Company'] = df['Company'].map(company_mapping)
+
+        # Display the cleaned DataFrame
+        print(df[['Company', 'Simplified Company']])
+
+
     def generate_dataframe(self):
         if self.file_paths:
             try:
                 dfs = []
                 for file_path in self.file_paths:
-                    print(os.path.splitext(os.path.basename(file_path)))
+                    #print(os.path.splitext(os.path.basename(file_path)))
                     df = pd.read_csv(file_path, skiprows=3)
 
                     df['SourceFile'] = file_path #name of linkedin data is currently derrived from the existing files name (hence name files as people)
                     dfs.append(df)
 
                 combined_df = pd.concat(dfs, ignore_index=True)
-
-                # TODO: where to work on generating the graphs
-                print("Combined DataFrame:")
-                print(combined_df)
+                #self.simplify_companies(combined_df) - JUST ignore this for the timing being
 
             except Exception as e:
                 print("Error opening the files with pandas:", e)
+
+    #def create_graph(self):
+        #TODO: generate graphs here
 
 # Creating the main window and running it
 root = tk.Tk()
